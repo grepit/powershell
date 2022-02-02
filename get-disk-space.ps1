281fd14e883d207e6ad169cf25a2a26f20bbde07
grepit <aghamoa@yahoo.com>
2022-02-02 10:02:11 -0800

    get disk space

---
+++ get-disk-space.ps1
@@ -0,0 +1,22 @@
+$props = @(
+    'DriveLetter'
+    'FileSystemLabel'
+    'FileSystem'
+    'DriveType'
+    'HealthStatus'
+    'OperationalStatus'
+    @{
+        Name = 'SizeRemaining'
+        Expression = { "{0:N3} Gb" -f ($_.SizeRemaining/ 1Gb) }
+    }
+    @{
+        Name = 'Size'
+        Expression = { "{0:N3} Gb" -f ($_.Size / 1Gb) }
+    }
+    @{
+        Name = '% Free'
+        Expression = { "{0:P}" -f ($_.SizeRemaining / $_.Size) }
+    }
+)
+
+Get-Volume -DriveLetter C, D | Select-Object $props | Format-Table
