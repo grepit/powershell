2ddd558d08671dabe2ae871547324db509ad2b1b
grepit <aghamoa@yahoo.com>
2022-05-16 14:15:20 -0700

    Update install_chrome.ps1

--- install_chrome.ps1
+++ install_chrome.ps1
@@ -1,4 +1,4 @@
-$uri = "https://dl.google.com/chrome/install/ChromeStandaloneSetup64.exe"
+$uri = "https://dl.google.com/chrome/install/latest/chrome_installer.exe"
 $path = "$PSScriptRoot\ChromeStandaloneSetup64.exe" 
 Invoke-WebRequest -Uri $uri -OutFile $path
 & $path /install
