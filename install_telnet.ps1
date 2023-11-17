db5b4c35b1be4523a94e14c33220f7c1d14c6336
grepit <aghamoa@yahoo.com>
2023-11-16 20:32:45 -0800

    Update install_telnet.ps1

--- install_telnet.ps1
+++ install_telnet.ps1
@@ -1,1 +1,6 @@
 dism /online /Enable-Feature /FeatureName:TelnetClient
+## if above does not work you can try this:
+Enable-WindowsOptionalFeature -Online -FeatureName "TelnetClient"
+
+## another approach
+dism /online /Enable-Feature /FeatureName:TelnetClient
