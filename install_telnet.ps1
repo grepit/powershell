10eea05bcf173f60a7d436aefe96829eb4066ab3
grepit <aghamoa@yahoo.com>
2024-04-04 07:35:01 -0700

    Update install_telnet.ps1

--- install_telnet.ps1
+++ install_telnet.ps1
@@ -4,3 +4,5 @@ Enable-WindowsOptionalFeature -Online -FeatureName "TelnetClient"
 
 ## another approach
 dism /online /Enable-Feature /FeatureName:TelnetClient
+# yoo can also use this instead of telnet
+Test-NetConnection some_ip_here -Port 22
